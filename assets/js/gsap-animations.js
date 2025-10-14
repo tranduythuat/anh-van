5fd4a7dd2463474a88b5c928d32bedfe63856288
tranduythuat <thuattran1810@gmail.com>
2025-10-14 11:50:30 +0700

    Update animation

--- assets/js/gsap-animations.js
+++ assets/js/gsap-animations.js
@@ -18,22 +18,3 @@ function gsapFlipIn(selector) {
     });
   });
 }
-function gsapFadeIn(selector) {
-  gsap.utils.toArray(selector).forEach((el) => {
-    gsap.fromTo(
-      el,
-      { opacity: 0, y: 50 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 1,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: el,
-          start: "top 85%",
-          toggleActions: "play none none none",
-        },
-      }
-    );
-  });
-}
