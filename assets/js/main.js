e2bd233ba9b64cf2562842c157cb974588c7a4d0
unknown <thuattran1810@gmail.com>
2025-10-19 16:06:14 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,98 @@
+async function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log("🚀 ~ handleFormSubmit ~ data:", data);
+
+  const {
+    name: name,
+    attendance: confirm,
+    message: message,
+  } = data;
+  console.log("🚀 ~ handleFormSubmit 2~ data:", data);
+
+  // Thông báo khi bắt đầu gửi
+  Swal.fire({
+    title: "Đang gửi /Sending/...",
+    text: "Vui lòng chờ trong giây lát /Please wait a moment/",
+    icon: "info",
+    allowOutsideClick: false,
+    didOpen: () => {
+      Swal.showLoading();
+    },
+  });
+
+  const url =
+    "";
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        name,
+        confirm,
+        message,
+      }),
+    });
+
+    const result = await res.json().catch(() => ({}));
+    console.log("Server response:", result);
+
+    form.reset();
+
+    // Thông báo thành công
+    Swal.fire({
+      title: "Thành công /Success/!",
+      text: "Cảm ơn bạn đã gửi phản hồi, thông tin đã được gửi đến dâu rể rồi nha /Thank you for your feedback, the information has been sent to the bride and groom./",
+      icon: "success",
+      confirmButtonText: "OK",
+      confirmButtonColor: "#3f4122ff",
+    });
+  } catch (error) {
+    console.error("Error:", error);
+
+    // Thông báo lỗi
+    Swal.fire({
+      title: "Lỗi!",
+      text: "OPPS! Đã xảy ra lỗi: " + error.message,
+      icon: "error",
+      confirmButtonText: "Thử lại",
+      confirmButtonColor: "#3f4122ff",
+    });
+  }
+}
+
+async function toggleMusic(e) {
+  const audio = document.getElementById('audio');
+  const iconSvg = document.getElementById('iconSvg');
+  if (!audio.src) {
+    alert('Chưa có nhạc, vui lòng thêm src cho audio.');
+    return;
+  }
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+
+  audio.addEventListener('play', () => {
+    iconSvg.classList.add('spin');
+  });
+  audio.addEventListener('pause', () => {
+    iconSvg.classList.remove('spin');
+  });
+}
+
+
 // Kích hoạt ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
 
 // Gọi các hiệu ứng có sẵn
 document.addEventListener("DOMContentLoaded", () => {
   gsapFlipIn(".animate-flip");
-  gsapFlipInAndSwayEffect(".sway-wrapper");
-  
   const thumbSwiper = new Swiper(".thumb-swiper", {
     spaceBetween: 10,
     slidesPerView: 4,
@@ -31,4 +118,12 @@ document.addEventListener("DOMContentLoaded", () => {
     fadeEffect: { crossFade: true },
     speed: 1000 // tốc độ chuyển (ms)
   });
+
+  const btn = document.getElementById('player-btn');
+  btn.addEventListener('click', toggleMusic);
+
+  const form = document.forms["rsvpForm"];
+  if (form) {
+    form.addEventListener("submit", handleFormSubmit);
+  }
 });
